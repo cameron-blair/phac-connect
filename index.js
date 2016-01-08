2a5ec6ea890fbe76577e28cb8ab3e8e0cf2712b6
cameron-blair <cblair@cegep-heritage.qc.ca>
2016-01-08 13:50:25 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,83 @@
+var app = require('express')();
+var server = require('http').createServer(app);
+var io = require('socket.io').listen(server);
+var socketioJwt = require('socketio-jwt');
+var jwt = require('express-jwt');
+var mongoose = require('mongoose');
+
+server.listen(process.env.PORT || 3001);
+
+var jwtCheck = jwt({
+  secret: new Buffer('HO_BSpKmYZWaYuXRbhuC0zbDUE6dWeMLkdqVTrOzvV8wmMnwBgj8vijMHPBsXVwe', 'base64'),
+  audience: 'TjWERMTxpeB9snWo1rSRjLrEhPNNWziz'
+});
+
+mongoose.connect('mongodb://localhost/chat', function(error) {
+	if (error) {
+		console.log(error);
+	}
+	else {
+		console.log('[MongoDB] Connection: Success');
+	}
+});
+
+var chatSchema = mongoose.Schema({
+	user: String,
+	msg: String,
+	type: String,
+	created: {type: Date, default: Date.now}
+	});
+	
+var Chat = mongoose.model('Message', chatSchema);
+
+app.use('/index.html', jwtCheck);
+
+app.get('/', function(req, res) {
+      res.sendFile(__dirname + '/index.html');  
+    });
+	
+
+app.get('/images/gov.png', function(req, res){
+   res.sendFile(__dirname + '/images/gov.png');
+});
+
+app.get('/scripts/script.js', function(req, res){
+   res.sendFile(__dirname + '/scripts/script.js');
+});
+
+app.get('/styles/style.css', function(req, res){
+   res.sendFile(__dirname + '/styles/style.css');
+});
+
+app.get('/users/users.txt', function(req, res){
+   res.sendFile(__dirname + '/users/users.txt');
+});
+
+app.get('/images/tweet.png', function(req, res){
+   res.sendFile(__dirname + '/images/tweet.png');
+});
+
+app.get('/users/av/Cameron.png', function(req, res){
+   res.sendFile(__dirname + '/users/av/Cameron.png');
+});
+
+io.sockets.on('connection', socketioJwt.authorize({
+    secret: Buffer('HO_BSpKmYZWaYuXRbhuC0zbDUE6dWeMLkdqVTrOzvV8wmMnwBgj8vijMHPBsXVwe', 'base64'),
+    timeout: 15000 // 15 seconds to send the authentication message
+  })).on('authenticated', function(socket) {
+  });
+  
+io.sockets.on('connection', function(socket) {
+	var query = Chat.find({});
+	query.sort('-created').limit(10).exec(function(err,msgs) {
+		if(err) throw err;
+		socket.emit('load history', msgs);
+	});
+	socket.on('chat message', function(msg){
+		var newMsg = new Chat({user: msg.user, msg: msg.msg, type: msg.type});
+		newMsg.save(function(err){
+			if(err) throw err;
+			io.sockets.emit('chat message', msg);
+		});
+	});
+});
---
+++ scripts/script.js
@@ -0,0 +1,100 @@
+var lock = null;
+			$(document).ready(function() {
+				lock = new Auth0Lock('TjWERMTxpeB9snWo1rSRjLrEhPNNWziz', 'phacconnect.auth0.com');
+			});
+			var userProfile;
+			var msgText = "";
+			var type = "";
+			var userToken;
+			var username = "Guest";
+			$('#login').click(function(e) {
+				e.preventDefault();
+				lock.show(function(err, profile, token) {
+					if (err) {
+						alert('There was an error');
+						alert(err);
+					} else {
+						$('#control').css("display", "");
+						$('#loginMsg').css("display", "none");
+						userToken = token;
+						localStorage.setItem('userToken', token);
+						userProfile = profile;
+						$('#login').html(profile.email);
+						var emailArr = profile.email.split("@");
+						username = emailArr[0];
+					}
+				})
+			});
+			var socket = io();
+			$('form').submit(function(e) {
+			e.preventDefault();
+				if ($('#m').val() !== "") {
+					msgText = $('#m').val();
+					type = $('#ddlMsgType').val();
+					socket.emit('chat message', {user: username, msg: msgText, type: type});
+					$('#m').val('');
+					$('#ddlMsgType').val("General");
+				}
+				return false;
+			});
+			socket.on('connect', function() {}).emit('authenticate', {
+				token: userToken
+			}); // send the jwt
+			
+			socket.on('load history', function(msgs){
+				for(var i=msgs.length-1; i >= 0; i--){
+					handleMsg(msgs[i]);
+				}
+			});
+
+			socket.on('chat message', function(msg) {
+				handleMsg(msg);
+			});
+			
+			function handleMsg(msg) {
+				var u = msg.user;
+				var style = msg.type;
+				var userMsg = msg.msg;
+				userMsg = userMsg.replace(/;/g, "");
+				userMsg = userMsg.replace(/&/g, "and");
+				var styleString = '';
+				var numDivs = $('.msgSpan').length;
+				switch (style) {
+					case "Government":
+						styleString = 'style="background-color:';
+						styleString += '#FFE74C"';
+						break;
+					case "Department":
+						styleString = 'style="background-color:';
+						styleString += '#6BF178"';
+						break;
+					case "Group":
+						styleString = 'style="background-color:';
+						styleString += '#35A7FF"';
+						break;
+				}
+				if (styleString === '') {
+					if (numDivs % 2 == 0)
+						styleString = 'style="background-color:#fff"';
+					else
+						styleString = 'style="background-color:#bbb"';
+				}
+
+				var iden = '<span><span style="font-weight:bold;">' + u + '</span>: <span class="msgSpan">';
+				iden = '<img src="../users/av/Cameron.png" style="height:20px;width:20px;border-width:2px;border-style:solid;border-color:#333;border-radius:25px;margin-right:5px;float:left;"/>' + iden;
+				$('#messages').prepend($('<div onmouseout="hideButton(this)" onmouseover="showButton(this)" ' + styleString + '>').html(iden + "</span>"));
+				$('.msgSpan').first().text(userMsg); // This is to ensure no html can be applied to the messages.
+				var share = "<span style='margin-left:10px;'>";
+				share += "<span id=\"imgSpan\" style=\"visibility:hidden\"><a target='_blank' style='display:inline-block;' href=\"https://twitter.com/intent/tweet?&text=" + userMsg + " - Shared via PHAC Connect\"><button style='border-radius:5px;color:#333;font-size:.8em;border:1px solid #aaa'><img style=\"width:18px;height:12px;\" src=\"images/tweet.png\"/> Share</button></a></span></span>";
+				var msg = $('#messages div').first().html() + share + "</span>";
+				$('#messages div').first().html(msg);
+				
+			}
+
+			function showButton(span) {
+				span.querySelector("#imgSpan").style.visibility = "visible";
+			}
+
+			function hideButton(span) {
+				span.querySelector("#imgSpan").style.visibility = "hidden";
+			}
