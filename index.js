443615777f79c72f75b08be52afcab2b71f2f586
cameron-blair <cblair@cegep-heritage.qc.ca>
2016-03-24 17:36:04 -0400

    Revised look and feel again

--- index.js
+++ index.js
@@ -4,16 +4,16 @@ var io = require('socket.io').listen(server);
 var socketioJwt = require('socketio-jwt');
 var jwt = require('express-jwt');
 var mongoose = require('mongoose');
-//server.listen(process.env.OPENSHIFT_NODEJS_PORT || 8080, process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1');
-server.listen(3001);
+server.listen(process.env.OPENSHIFT_NODEJS_PORT || 8080, process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1');
+//server.listen(3001);
 
 var jwtCheck = jwt({
   secret: new Buffer('HO_BSpKmYZWaYuXRbhuC0zbDUE6dWeMLkdqVTrOzvV8wmMnwBgj8vijMHPBsXVwe', 'base64'),
   audience: 'TjWERMTxpeB9snWo1rSRjLrEhPNNWziz'
 });
 
-//mongoose.connect(process.env.OPENSHIFT_MONGODB_DB_URL);
-mongoose.connect('mongodb://localhost/messages');
+mongoose.connect(process.env.OPENSHIFT_MONGODB_DB_URL);
+//mongoose.connect('mongodb://localhost/messages');
 
 var chatSchema = mongoose.Schema({
 	user: String,
